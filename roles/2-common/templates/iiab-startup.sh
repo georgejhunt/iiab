d0278ebc3a5bec1e50074eeccb797a36f1c20c2c
George Hunt <georgejhunt@gmail.com>
2018-02-22 20:33:19 +0000

    add the hotspot-off action to startup.sh

--- roles/2-common/templates/iiab-startup.sh
+++ roles/2-common/templates/iiab-startup.sh
@@ -33,6 +33,12 @@ if [[ $(grep -i raspbian /etc/*release) != "" ]]; then
       if [[ $(grep "^hostapd_enabled = True" /etc/iiab/iiab.ini) ]]; then
          ip link set dev wlan0 promisc on
       fi
+   else
+      # will fail if supplicant is already running
+      /usr/bin/killall wpa_supplicant
+      /sbin/wpa_supplicant -iwlan0 -c/etc/wpa_supplicant/wpa_supplicant.conf &
+      sleep 1
+      dhclient wlan0
    fi
 
 fi
