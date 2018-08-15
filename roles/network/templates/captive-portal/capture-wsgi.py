9b6dfcda72dcdbee81024d6d2a70979b2f8a9cfb
George Hunt <georgejhunt@gmail.com>
2018-08-15 01:58:32 +0000

    catch urls debugged

--- roles/network/templates/captive-portal/capture-wsgi.py
+++ roles/network/templates/captive-portal/capture-wsgi.py
@@ -23,42 +23,49 @@ def tstamp(dtime):
 def update_user(ip, mac, newts,ymd):
     fh, target_file_path = mkstemp()
     with open(target_file_path, 'w') as target_file:
-        with open(/opt/iiab/captive-portal/users, 'r') as source_file:
+        with open('/opt/iiab/captive-portal/users', 'r') as source_file:
             for line in source_file:
-                target_file.write(line.replace(pattern, substring))
-                os.remove(source_file_path)
-                move(target_file_path, source_file_path)
+                if ip in line:
+                    target_file.write("%s %s %8.0d %s" % ip,mac,ts,ymd)
+                else:
+                    target_file.write(line)
+            os.remove(source_file_path)
+            move(target_file_path, source_file_path)
 
-def replace(source_file_path, pattern, substring):
-fh, target_file_path = mkstemp()
-with open(target_file_path, 'w') as target_file:
-            with open(source_file_path, 'r') as source_file:
-                            for line in source_file:
-                                                target_file.write(line.replace(pattern, substring))
-                                                    remove(source_file_path)
-                                                        move(target_file_path, source_file_path)
-                                                                                                                                    
+def microsoft(environ,start_response):
+    response_body = "This worked"
+    status = '302 Moved Temporarily'
+    response_headers = [('Location','http://box.lan/home')]
+    start_response(status, response_headers)
+    return [response_body]
+
+def android(environ, start_response):
+    response_body = "This worked"
+    status = '302 Moved Temporarily'
+    response_headers = [('Location','http://box.lan/home')]
+    start_response(status, response_headers)
+    return [response_body]
+
+def macintosh(environ, start_response):
+    response_body = "This worked"
+    status = '302 Moved Temporarily'
+    response_headers = [('Location','http://box.lan/home')]
+    start_response(status, response_headers)
+    return [response_body]
+
 def application (environ, start_response):
     global CATCH
+    global LIST
     global EXPIRE_SECONDS
-    ip = environ['HTTP_X_FORWARDED_FOR'].strip()
-    cmd="arp -an %s|gawk \'{print $4}\'" % ip
-    mac = subprocess.check_output(cmd, shell=True)
 
     # Sorting and stringifying the environment key, value pairs
-    '''    data = [
-        '%s: %s\n' % (key, value) for key, value in sorted(environ.items())
-    ]
-    '''
-    data = []
-    data.append("host: %s\n"%environ['HTTP_HOST'])
-    data.append("path: %s\n"%environ['PATH_INFO'])
-    data.append("query: %s\n"%environ['QUERY_STRING'])
-    data.append("ip: %s\n"%environ['HTTP_X_FORWARDED_FOR'])
-    logging.debug(data)
-    cmd="sudo iptables -I internet 1 -t mangle -m mac --mac-source %s -j RETURN"%mac
-    print(cmd)
+    if LIST:
+        data = [
+        '%s: %s\n' % (key, value) for key, value in sorted(environ.items()) ]
+        logging.debug(data)
+    
     if CATCH:
+        logging.debug("Checking for url %s"%environ['HTTP_HOST'])
         found = False
         if os.path.exists("/opt/iiab/captive-portal/checkurls"):
            with open("/opt/iiab/captive-portal/checkurls","r") as checkers:
@@ -69,25 +76,55 @@ def application (environ, start_response):
         if not found:
            with open("/opt/iiab/captive-portal/checkurls","a") as checkers:
                outstr ="%s\n" %  (environ['HTTP_HOST']) 
-               users.write(outstr)
-
+               checkers.write(outstr)
     else:
+        ip = environ['HTTP_X_FORWARDED_FOR'].strip()
+        cmd="arp -an %s|gawk \'{print $4}\'" % ip
+        mac = subprocess.check_output(cmd, shell=True)
+        data = []
+        data.append("host: %s\n"%environ['HTTP_HOST'])
+        data.append("path: %s\n"%environ['PATH_INFO'])
+        data.append("query: %s\n"%environ['QUERY_STRING'])
+        data.append("ip: %s\n"%environ['HTTP_X_FORWARDED_FOR'])
+        logging.debug(data)
+        cmd="sudo iptables -I internet 1 -t mangle -m mac --mac-source %s -j RETURN"%mac
+        logging.debug(cmd)
         found = False
         if os.path.exists("/opt/iiab/captive-portal/users"):
            with open("/opt/iiab/captive-portal/users","r") as users:
+              ts=tstamp(datetime.datetime.now(tzutc()))
               for line in users:
                  #print line, ip
                  if ip in line:
                     session_start = int(line.explode(' ')[2])
-                    ts=tstamp(datetime.datetime.now(tzutc()
                     if ts - session_start < EXPIRE_SECONDS:
                         found = True
                         break
         if not found:
-           ts=tstamp(datetime.datetime.now(tzutc()
            ymd=datetime.datetime.today().strftime("%y%m%d-%H%M")
            update_user(ip,mac.strip(),ts,ymd)
 
+        if environ['HTTP_HOST'] == "captive.apple.com" or
+           environ['HTTP_HOST'] == "appleiphonecell.com":
+           environ['HTTP_HOST'] == "detectportal.firefox.com" or
+           environ['HTTP_HOST'] == "*.apple.com.edgekey.net" or
+           environ['HTTP_HOST'] == "gsp1.apple.com" or
+           environ['HTTP_HOST'] == "apple.com" or
+           environ['HTTP_HOST'] == "www.apple.com" or
+           return macintosh(environment, start_response) 
+
+        if environ['HTTP_HOST'] == "clients3.google.com" or
+           environ['HTTP_HOST'] == "connectivitycheck.gstatic.com":
+           return android(environment, start_response) 
+           
+        if environ['HTTP_HOST'] == "ipv6.msftncsi.com" or
+           environ['HTTP_HOST'] == "ipv6.msftncsi.com.edgesuite.net" or
+           environ['HTTP_HOST'] == "www.msftncsi.com" or
+           environ['HTTP_HOST'] == "www.msftncsi.com.edgesuite.net" or
+           environ['HTTP_HOST'] == "www.msftconnecttest.com" or
+           environ['HTTP_HOST'] == "teredo.ipv6.microsoft.com" or
+           environ['HTTP_HOST'] == "teredo.ipv6.microsoft.com.nsatc.net" or
+           return microsoft(environment, start_response) 
     response_body = "This worked"
     status = '302 Moved Temporarily'
     response_headers = [('Location','http://box.lan/home')]
@@ -103,9 +140,14 @@ from iiab_env import get_iiab_env
 logging.basicConfig(filename='/var/log/apache2/portal.log',format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M',level=logging.DEBUG)
 if len(sys.argv) > 1 and sys.argv[1] == '-d':
     CATCH = True
+    LIST = False
     PORT=80
+elif len(sys.argv) > 1 and sys.argv[1] == '-l':
+    LIST = True
+    CATCH = False
 else:
     CATCH = False
+    LIST = False
     PORT=9090
 # Instantiate the server
 httpd = make_server (
@@ -117,5 +159,5 @@ httpd = make_server (
 # Wait for a single request, serve it and quit
 #httpd.handle_request()
 httpd.serve_forever()
-# vim: tabstop=3 expandtab shiftwidth=3 softtabstop=3 background=light
+# vim: tabstop=3 expandtab shiftwidth=3 softtabstop=3 background=dark
 
