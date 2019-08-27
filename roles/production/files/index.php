c9cbc63affc61f1dac36793a67b2b23f32dc4f63
George Hunt <georgejhunt@gmail.com>
2019-08-27 12:07:10 -0700

    directory mostly working:

--- roles/production/files/index.php
+++ roles/production/files/index.php
@@ -52,34 +52,42 @@ $iter=new RecursiveDirectoryIterator($video_base);
 $bytestotal=0;
 $nbfiles=0;
 $menuhtml =  '<div class="content-item" >';
-foreach (new RecursiveIteratorIterator($iter) as $filename=>$cur) {
-  if (is_dir($cur) && substr($cur,-1,1) != '.'){
-    $heading = basename($cur);
-    $menuhtml .= "<h1>$heading</h1>";
+$dir_list = array();
+$treeIter = new RecursiveIteratorIterator($iter);
+foreach ($treeIter as $filename=>$cur) {
+  //$menuhtml .= $cur . '<br>';
+  if (is_dir($cur)) {
+    $regex = "@.*/videos/([A-Za-z0-9-_.]+)/\.$@";
+    preg_match($regex,$cur,$matches);
+    if ($matches) {
+      if (substr($matches[1],0,5) == 'group'){
+        $heading = substr($matches[1],6);
+      } else $heading = 'General';
+    } else continue;
+    $menuhtml .= "<h3>$heading</h3>";
     continue;
   }
-    $regex = "@/([A-Za-z0-9-_.]+/)([A-Za-z0-9-_.]+/)([A-Za-z0-9-_.]+)(\.mp4|m4v|mov)$@";
-    preg_match($regex,$cur,$matches);
-    if ( ! $matches ) continue;
-    //die(print_r($matches));
-    $fname = $matches[2] . $matches[3] . $matches[4];
-    $after_video = $fname;
-    if ($matches[1] !== 'videos') $after_video = "$matches[1]/$fname";
-    $href = $video_url . "/viewer.php?name=" . $after_video;
-    $category_dir = $matches[1]; 
-    if ($category_dir == 'videos/')  $category_dir = '';
-    $path = $video_base . "$category_dir/$matches[3]";
-    $title = getOneLine("$path/title");
-    if ($title === '') $title = $fname;
-    $video_link = "<a href=$href >$title</a>";
-    $oneliner = getOneLine("$path/oneliner");
-    $filesize=$cur->getSize();
-    $bytestotal+=$filesize;
-    $nbfiles++;
-    $pretty = human_filesize($filesize);
-    $video_time = getDuration($filename);
-    $modate = date ("F d Y", filemtime($filename));
-    $menuhtml .= "<br>$video_link -- $oneliner <br>   $pretty, Duration: $video_time h:m:s, $modate<br>";
+  $regex = "@(/[A-Za-z0-9-_.]+/)([A-Za-z0-9-_.]+/)([A-Za-z0-9-_.]+)(\.mp4|m4v|mov)$@"; preg_match($regex,$cur,$matches);
+  if ( ! $matches ) continue;
+  //$menuhtml .= (print_r($matches));
+  $fname = $matches[2] . $matches[3] . $matches[4];
+  $after_video = $fname;
+  if ($matches[1] !== '/videos/') $after_video = $matches[1].$fname;
+  $href = $video_url . "/viewer.php?name=" . $after_video;
+  $category_dir = $matches[1]; 
+  if ($category_dir == '/videos/')  $category_dir = '';
+  $path = $video_base . "$category_dir/$matches[3]";
+  $title = getOneLine("$path/title");
+  if ($title === '') $title = $fname;
+  $video_link = "<a href=$href >$title</a>";
+  $oneliner = getOneLine("$path/oneliner");
+  $filesize=$cur->getSize();
+  $bytestotal+=$filesize;
+  $nbfiles++;
+  $pretty = human_filesize($filesize);
+  $video_time = getDuration($filename);
+  $modate = date ("F d Y", filemtime($filename));
+  $menuhtml .= "<br>$video_link -- $oneliner <br>   $pretty, Duration: $video_time h:m:s, $modate<br>";
 }
 $bytestotal=human_filesize($bytestotal);
 $menuhtml .= "<br>Total: $nbfiles files,  $bytestotal . bytes\n";
