53095b38ab579fba41e9105ae11adf1109bcc1a4
George Hunt <georgejhunt@gmail.com>
2021-02-15 16:30:22 +0000

    small improvements

--- roles/emule/files/dovecot/setup_dovecot.sh
+++ roles/emule/files/dovecot/setup_dovecot.sh
@@ -1,1 +1,6 @@
+cat <<EOL >mysql -uroot
+CREATE DATABASE IF NOT EXISTS email_accounts;
+grant ALL on email_accounts.* to "Admin"@"localhost" identified by "changeme";
+FLUSH PRIVILEGES;
+EOL
 mysql -uAdmin -pchangeme < ./mysql.initial.sql
